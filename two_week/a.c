0e8bf33b3a578fb1ae10b81333dc7e99370f8f38
luokun <455063468@qq.com>
2022-04-15 14:30:42 +0800

    second

--- two_week/a.c
+++ two_week/a.c
@@ -3,9 +3,9 @@ void function_b();
 int main()
 {
 	int offset;
-	char str1[64] , str2[64];
+	char str1[64] , str2[64],s[64];
 	printf("请输入：文件1，文件2，偏移值。\n");
-	scanf("%s%s%d",str1,str2,&offset);
-	function_b(str1,str2,offset);
+	scanf("%s%s%s",str1,str2,s);
+	function_b(str1,str2,s);
 	return 0;
 }
--- two_week/b.c
+++ two_week/b.c
@@ -1,27 +1,33 @@
 #include     <stdio.h>
-void function_b(char *filename1,char *filename2,int offset)
+void function_b(char *filename1,char *filename2,char s[])
 {
         int ret;
-        int n=0,i=0;
+        int n=0,i=0,t=0;
         FILE *fp2=fopen(filename2,"rb");
         FILE *fp1=fopen(filename1,"rb+");
         char ch;
+        long sum=0;
+  for(i=2;s[i];i++)
+  {
+            if(s[i]<='9')
+                    t=s[i]-'0';
+            else
+                    t=s[i]-'A'+10;
+            sum=sum*16+t;
+    }
 
-        fseek(fp1,offset,SEEK_SET);
-
+        fseek(fp1,sum,SEEK_SET);
         while((ch=fgetc(fp2))!=EOF)
                 n++;
-
         fseek(fp2,0,SEEK_SET);
         for(i=0;i<n;i++)
         {
                 ch=fgetc(fp2);
                 fputc(ch,fp1);
                ret=ftell(fp2);
-               printf("%d\n",ret);
+               printf("已经拷贝第%d字节\n",ret);
         }
         fclose(fp1);
         fclose(fp2);
         printf("b.c执行完成\n");
-
 }
